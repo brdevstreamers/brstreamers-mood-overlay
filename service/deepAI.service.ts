f9a5398424060d9fa511f567d589173427da36fe
Flavio Mendes <flaviojmendes@gmail.com>
2023-01-15 11:16:27 +0000

    changing image to openai

--- service/deepAI.service.ts
+++ service/deepAI.service.ts
@@ -1,13 +1,25 @@
 import deepai from "deepai";
+import { Configuration, OpenAIApi } from "openai";
 import { BotStorage } from "../chatScore";
-import { DEEPAI_API_KEY } from "../config";
+import { DEEPAI_API_KEY, OPENAPI_KEY } from "../config";
 export class DeepAIService {
   static async generate(input: string) {
     deepai.setApiKey(DEEPAI_API_KEY);
-    var resp = await deepai.callStandardApi("text2img", {
-      text: input,
+    // var resp = await deepai.callStandardApi("text2img", {
+    //   text: input,
+    // });
+    const configuration = new Configuration({
+      apiKey: OPENAPI_KEY,
     });
-    BotStorage.getInstance().setLatestImageURL(resp.output_url)
-    return resp.output_url
+    
+    const openai = new OpenAIApi(configuration);
+
+    const response = await openai.createImage({
+      prompt: input,
+      n: 1,
+      size: "1024x1024",
+    });
+    BotStorage.getInstance().setLatestImageURL(response.data.data[0].url || '')
+    return response.data.data[0].url
   }
 }
